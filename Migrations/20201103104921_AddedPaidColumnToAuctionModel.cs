206d3a5bdc96ac615c4cb8ac8cffa904ecef87d3
Amalia <amalianibarra@gmail.com>
2020-11-03 05:52:30 -0500

    Add Paid column and OrderBidTable trough migration
    From now on every change to the model must be followed by a migration to
    keep track of changes.
    Note from las commit: Change password and edit profile picture are now
    separate from the edit form in User/Edit.

---
+++ Migrations/20201103104921_AddedPaidColumnToAuctionModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrendyShop.Migrations
+{
+    public partial class AddedPaidColumnToAuctionModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Paid",
+                table: "Auctions",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "AuctionOrders",
+                columns: table => new
+                {
+                    Date = table.Column<DateTime>(nullable: false),
+                    CustomerId = table.Column<string>(nullable: false),
+                    ArticleId = table.Column<int>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    ShippingCharge = table.Column<float>(nullable: false),
+                    Paid = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuctionOrders", x => new { x.Date, x.CustomerId, x.ArticleId });
+                    table.ForeignKey(
+                        name: "FK_AuctionOrders_Auctions_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "Auctions",
+                        principalColumn: "ArticleId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuctionOrders_AspNetUsers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuctionOrders_ArticleId",
+                table: "AuctionOrders",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuctionOrders_CustomerId",
+                table: "AuctionOrders",
+                column: "CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuctionOrders");
+
+            migrationBuilder.DropColumn(
+                name: "Paid",
+                table: "Auctions");
+        }
+    }
+}
